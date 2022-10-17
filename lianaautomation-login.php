82902e11156be5a560183611fb40c1857316babf
Jaakko Pero <jaakko.pero@lianatech.com>
2022-10-17 15:06:46 +0300

    added license, readme, phpcs standards and some initial changes

--- lianaautomation-login.php
+++ lianaautomation-login.php
@@ -1,24 +1,20 @@
 <?php
 /**
- * Plugin Name:       LianaAutomation - Login
- * Plugin URI:        https://www.lianatech.com/solutions/websites
- * Description:       LianaAutomation for WordPress (login tracking)
- * Version:           1.0.3
+ * Plugin Name:       LianaAutomation Login
+ * Description:       LianaAutomation Login Tracking for WordPress
+ * Version:           1.0.4
  * Requires at least: 5.2
  * Requires PHP:      7.4
- * Author:            Liana Technologies
+ * Author:            Liana Technologies Oy
  * Author URI:        https://www.lianatech.com
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0-standalone.html
  * Text Domain:       lianaautomation
  * Domain Path:       /languages
- * 
+ *
  * PHP Version 7.4
- * 
- * @category Components
+ *
  * @package  WordPress
- * @author   Liana Technologies <websites@lianatech.com>
- * @author   Jaakko Pero <jaakko.pero@lianatech.com>
  * @license  https://www.gnu.org/licenses/gpl-3.0-standalone.html GPL-3.0-or-later
  * @link     https://www.lianatech.com
  */
@@ -26,14 +22,16 @@
 /**
  * Include cookie handler code
  */
-require_once dirname(__FILE__) . '/includes/lianaautomation-cookie.php';
+require_once dirname( __FILE__ ) . '/includes/lianaautomation-cookie.php';
 
 /**
  * Include WordPress login handler code
  */
-require_once dirname(__FILE__) . '/includes/lianaautomation-login.php';
+require_once dirname( __FILE__ ) . '/includes/lianaautomation-login.php';
 
 /**
- * Include admin panel code
+ * Conditionally include admin panel code
  */
-require_once dirname(__FILE__) . '/admin/lianaautomation-admin.php';
+if ( is_admin() ) {
+	require_once dirname( __FILE__ ) . '/admin/class-lianaautomation-login.php';
+}
